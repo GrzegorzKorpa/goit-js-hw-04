fada7cc5913282f6229fb583d75a161fa59593c1
GrzegorzKorpa <grzegorz.korpa@post.pl>
2024-05-25 12:46:00 +0200

    index.html

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,2 @@
 'use strict';
 
-function makeArray(firstArray, secondArray, maxLenhth) {
-
-    const temporaryArray = firstArray.concat(secondArray);
-    const finalArray = temporaryArray.slice(0, maxLenhth);
-
-  return finalArray;
-
-}
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
