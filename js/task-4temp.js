c5ee26756ba5dcb9da131abd2b58e4121ecaa23d
GrzegorzKorpa <grzegorz.korpa@post.pl>
2024-05-29 13:46:31 +0200

    Update task-4temp.js

--- js/task-4temp.js
+++ js/task-4temp.js
@@ -2,28 +2,7 @@
 
 function calcAverageCalories(days) {
 
-    // Nie udawało mi się działać bezpośrednio na 'days', 
-    // dlatego zdefiniowałem tablicę 'weekDays' zawierającą 'days'
-    
-    const weekDays = days;
-
-    // Poniżej zwrot 'calories' w postaci tablicy 'weekCalories';
-
-    const weekCalories = [];
-    for (const weekDay of weekDays) {
-        weekCalories.push(weekDay.calories);
-    }
-
-    // Sumowanie 'calories' z tablicy 'weekCalories';
-
-    let sum = 0;
-    for (let i = 0; i < weekCalories.length; i++) {
-        sum += weekCalories[i];
-    }
-    
-    // Poniżej wynik - obliczenie średniej
-    
-    return (sum / weekCalories.length);
+  return Boolean(calcAverageCalories);
 
     
 }
